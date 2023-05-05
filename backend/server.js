9fffa8ea3a319b90ab4438789f705871ca1ba501
Brad Traversy <traversymedia@gmail.com>
2023-05-05 10:39:30 -0400

    Prepare for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -19,7 +20,18 @@ app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
 
-app.get('/', (req, res) => res.send('API running'));
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
