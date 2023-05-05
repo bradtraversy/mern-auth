da3910443416c2f3662d449603baf0eaf68e8607
Brad Traversy <traversymedia@gmail.com>
2023-05-05 10:43:23 -0400

    fix

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './slices/AuthSlice';
+import authReducer from './slices/authSlice';
 import { apiSlice } from './slices/apiSlice';
 
 const store = configureStore({
